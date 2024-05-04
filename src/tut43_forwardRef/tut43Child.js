fcfe0b9ca8af2eb13d86f701de07b7b057030218
ProjectWithRavi <raviyt73@gmail.com>
2024-05-04 16:47:46 +0530

    forwardRef in class component

--- src/tut43_forwardRef/tut43Child.js
+++ src/tut43_forwardRef/tut43Child.js
@@ -4,14 +4,29 @@ make sure child component wrap in forwardRef , which we write in bottom
 */
 
 
-import React,{forwardRef} from 'react'
+import React,{Component, forwardRef} from 'react'
 
-const Tut43Child = (props,myref) => {
-  return (
-    <div>
-      <input className='mb-3' type="text" ref={myref}/>
-    </div>
-  )
-}
+// const Tut43Child = (props,myref) => {
+//   return (
+//     <div>
+//       <input className='mb-3' type="text" ref={myref}/>
+//     </div>
+//   )
+// }
 
-export default forwardRef( Tut43Child)
+// export default forwardRef( Tut43Child)
+
+
+//*********** when we want use forwardRef in class component */
+
+class Tut43Child extends Component {
+    render() {
+      return (
+        <div>
+          <input className='mb-3' type="text" ref={this.props.myref} />
+        </div>
+      );
+    }
+  }
+  
+export default forwardRef((props, ref) => <Tut43Child {...props} myref={ref} />);
